af03156f03f8a453f5defd75908b4f4263d05391
Clément Lafont <johnrazeur@gmail.com>
2025-06-17 12:47:21 +0000

    add clone

--- js-es/index.js
+++ js-es/index.js
@@ -1,31 +1,42 @@
-// Should add addBook, getBooksByAuthor, getTitles
+// Should add addBook, getBooksByAuthor, getTitles, openNewLibrary
 const library = {
-  booksStore: []
-}
-
+  booksStore: [],
+};
 
 // Should return title - author
 function getFormatedTitle(book) {
-  return ''
+  return "";
 }
 
 function fetchBooks() {
   return new Promise((resolve) => {
     setTimeout(() => {
       const books = [
-        { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
-        { title: '1984', author: 'George Orwell', year: 1949 },
-        { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
-      ]
-      resolve(books)
-    }, 1000)
-  })
+        {
+          title: "The Great Gatsby",
+          author: "F. Scott Fitzgerald",
+          year: 1925,
+        },
+        { title: "1984", author: "George Orwell", year: 1949 },
+        { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+      ];
+      resolve(books);
+    }, 1000);
+  });
 }
 
-// Exemple d'utilisation
 (async () => {
   // add books to library with the fetchBooks return
 
-  console.log(library.getTitles())
-  console.log(library.getBooksByAuthor('George Orwell'))
-})()
+  console.log(library.getTitles());
+  console.log(library.getBooksByAuthor("George Orwell"));
+
+  const newLibrary = library.openNewLibrary();
+  newLibrary.addBook({
+    title: "Foundation",
+    author: "Isaac Asimov",
+    year: 1951,
+  });
+
+  console.log(newLibrary.getTitles(), library.getTitles());
+})();
